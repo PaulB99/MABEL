34f149d984dd5dc317d054c6128cf077e5e1508e
Paul <paulbyrne99@rocketmail.com>
2020-11-25 01:28:13 +0200

    significant work
    Former-commit-id: 67f15498118218fce3cd6e5c291641266a4928d1

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# Main file to oversee bias detection and neutralisation
+
+def run(sent):
+    
+    # Evaluate bias
+    biased = True
+    
+    if biased:
+        # seq2seq neutralisation
+        sent = 'unbiased'
+        
+    return sent
