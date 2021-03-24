87c6b71c7f7091066f611b7b9c67e29505a8cb67
Paul <paulbyrne99@rocketmail.com>
2021-03-24 00:27:13 +0000

    pipeline expanded
    Former-commit-id: 5b379503553d8f687f09a4fdb6b1340a11f932e6

--- src/joint/run.py
+++ src/joint/run.py
@@ -1,8 +1,10 @@
 import torch 
 import sys
+import time
 sys.path.insert(0, '../')
 from taggers.base_model import model as t_model
 from neutralisers.bart import model as n_model
+from transformers import BertTokenizer, BartTokenizer
 
 
 # Program to load pretrained models and run the full pipeline
@@ -15,18 +17,29 @@ def load_ckpt(load_path, model):
 # 
 def pipeline(sentence, tagger='base_model', neutraliser='bart'):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    t_tokeniser = BertTokenizer.from_pretrained('bert-base-uncased')
+    n_tokeniser = BartTokenizer.from_pretrained("facebook/bart-base")
     
     # Load models
+    t1 = time.clock()
     tagger_path = '../../cache/taggers/' + tagger + '.pt'
     tagger_model = t_model.BERT().to(device)
     load_ckpt(tagger_path, tagger_model)
-    print('Tagger loaded!')
+    t2 = time.clock()
+    print('Tagger loaded in {}s!'.format(t2-t1))
     
+    t1 = time.clock()
     neutraliser_path = '../../cache/neutralisers/' + neutraliser + '.pt'
     neutraliser_model = n_model.BART().to(device)
     load_ckpt(neutraliser_path, neutraliser_model)
-    print('Neutraliser loaded!')
+    t2 = time.clock()
+    print('Neutraliser loaded in {}s!'.format(t2-t1))
     
+    t_tok = t_tokeniser(sentence)
+    biased = t_model(t_tok)
+    
+    if biased == 1:
+        print('Good')
 
 # Run
 pipeline('Example')
