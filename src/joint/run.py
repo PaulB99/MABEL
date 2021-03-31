4a5bdb50f9eb71a6fdd19582fde83fd627cd7c89
Paul <paulbyrne99@rocketmail.com>
2021-04-01 00:25:35 +0100

    command line input added
    Former-commit-id: 5924b2b8c6a1acebdba54fa19360823f740cdd56

--- src/joint/run.py
+++ src/joint/run.py
@@ -14,10 +14,7 @@ def load_ckpt(load_path, model):
     model.load_state_dict(torch.load(load_path))
 
 # The full detection and neutralisation pipeline
-def pipeline(sentence, tagger='base_model', neutraliser='bart'):
-    
-    # Split sentences
-    split_sent = sentence.split('.')
+def pipeline(tagger='base_model', neutraliser='bart'):
     
     # Device to run on
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -35,18 +32,7 @@ def pipeline(sentence, tagger='base_model', neutraliser='bart'):
     label_field = Field(sequential=False, use_vocab=False, batch_first=True, dtype=torch.float)
     text_field = Field(use_vocab=False, tokenize=t_tokeniser.encode, lower=False, include_lengths=False, batch_first=True, fix_length=MAX_SEQ_LEN, pad_token=PAD_INDEX, unk_token=UNK_INDEX)
     fields = [('labels', label_field), ('text', text_field)]
-    
-    # Populate dataset
-    prov_data = []
-    for x in split_sent:
-        ex = Example.fromlist([0,x], fields)
-        prov_data.append(ex)
-    sent_data = Dataset(prov_data, fields)
-        
-    print('Data initialised')
-    
-    iterator = BucketIterator(sent_data, batch_size=1, device=device, train=False, shuffle=False, sort=False, sort_key=lambda x: len(x.text))
-    print('Iterator initialised')
+    print('Initialised')
     
     # Load models
     t1 = time.clock()
@@ -63,65 +49,85 @@ def pipeline(sentence, tagger='base_model', neutraliser='bart'):
     t2 = time.clock()
     print('Neutraliser loaded in {}s!'.format(t2-t1))
     
-    # Output vals
-    output_array = []
-    biased_indices = []
-    ticker = -1
-    
-    # Check for bias
-    with torch.no_grad():
-        for (labels, text), _ in iterator:
-            ticker+=1
-            labels = labels.type(torch.LongTensor)           
-            labels = labels.to(device)
-            text = text.type(torch.LongTensor)  
-            text = text.to(device)
-            tagger_output = tagger_model(labels, text)
-            _,tagger_output = tagger_output
-            biased = torch.argmax(tagger_output, 1)
-            if biased == 1:
-                print('Biased!')
-                biased_indices.append(ticker)
-            elif biased == 0:
-                print('Unbiased!')
-                output_array.insert(ticker, split_sent[ticker])
-                
-                
-    # Prepare data for neutralisation
-    # Padding
-    MAX_SEQ_LEN = 128
-    PAD_INDEX = n_tokeniser.convert_tokens_to_ids(n_tokeniser.pad_token)
-    UNK_INDEX = n_tokeniser.convert_tokens_to_ids(n_tokeniser.unk_token)
-    
-    # Fields
-    text_field = Field(use_vocab=False, tokenize=n_tokeniser.encode, lower=False, include_lengths=False, batch_first=True, fix_length=MAX_SEQ_LEN, pad_token=PAD_INDEX, unk_token=UNK_INDEX)
-    fields = [('text', text_field)]
-    
-    # Populate dataset
-    prov_data = []
-    for x in split_sent:
-        ex = Example.fromlist([x], fields)
-        prov_data.append(ex)
-    sent_data = Dataset(prov_data, fields)
-    
-    iterator = BucketIterator(sent_data, batch_size=1, device=device, train=False, shuffle=False, sort=False, sort_key=lambda x: len(x.text))
-    print('Ready for neutralisation')
-    
-    # Neutralise sentences tagged as biased
-    ticker=-1
-    with torch.no_grad():
-        for (text), _ in iterator:
-            ticker+=1
-            text = text.type(torch.LongTensor)  
-            text = text.to(device)
-            neutraliser_output = neutraliser_model.generate(text)
-            decoded = n_tokeniser.decode(neutraliser_output[0], skip_special_tokens=True)
-            output_array.insert(ticker, decoded)
+    # Take input
+    sentence = str(input('Enter the phrase to be neutralised, or Exit to quit'))
+    while not sentence == 'Exit':
+        
+         # Split sentences
+        split_sent = sentence.split('.')
+        
+        # Populate dataset
+        prov_data = []
+        for x in split_sent:
+            ex = Example.fromlist([0,x], fields)
+            prov_data.append(ex)
+        sent_data = Dataset(prov_data, fields)    
+        print('Data initialised')
+        
+        iterator = BucketIterator(sent_data, batch_size=1, device=device, train=False, shuffle=False, sort=False, sort_key=lambda x: len(x.text))
+        print('Iterator initialised')
+        
+        # Output vals
+        output_array = []
+        biased_indices = []
+        ticker = -1
+        
+        # Check for bias
+        with torch.no_grad():
+            for (labels, text), _ in iterator:
+                ticker+=1
+                labels = labels.type(torch.LongTensor)           
+                labels = labels.to(device)
+                text = text.type(torch.LongTensor)  
+                text = text.to(device)
+                tagger_output = tagger_model(labels, text)
+                _,tagger_output = tagger_output
+                biased = torch.argmax(tagger_output, 1)
+                if biased == 1:
+                    print('Biased!')
+                    biased_indices.append(ticker)
+                elif biased == 0:
+                    print('Unbiased!')
+                    output_array.insert(ticker, split_sent[ticker])
+                    
+                    
+        # Prepare data for neutralisation
+        # Padding
+        MAX_SEQ_LEN = 128
+        PAD_INDEX = n_tokeniser.convert_tokens_to_ids(n_tokeniser.pad_token)
+        UNK_INDEX = n_tokeniser.convert_tokens_to_ids(n_tokeniser.unk_token)
+        
+        # Fields
+        text_field = Field(use_vocab=False, tokenize=n_tokeniser.encode, lower=False, include_lengths=False, batch_first=True, fix_length=MAX_SEQ_LEN, pad_token=PAD_INDEX, unk_token=UNK_INDEX)
+        fields = [('text', text_field)]
+        
+        # Populate dataset
+        prov_data = []
+        for x in split_sent:
+            ex = Example.fromlist([x], fields)
+            prov_data.append(ex)
+        sent_data = Dataset(prov_data, fields)
+        
+        iterator = BucketIterator(sent_data, batch_size=1, device=device, train=False, shuffle=False, sort=False, sort_key=lambda x: len(x.text))
+        print('Ready for neutralisation')
+        
+        # Neutralise sentences tagged as biased
+        ticker=-1
+        with torch.no_grad():
+            for (text), _ in iterator:
+                ticker+=1
+                text = text.type(torch.LongTensor)  
+                text = text.to(device)
+                neutraliser_output = neutraliser_model.generate(text)
+                decoded = n_tokeniser.decode(neutraliser_output[0], skip_special_tokens=True)
+                output_array.insert(ticker, decoded)
 
-    return output_array
+        #return output_array
+        print(output_array)
+        sentence = str(input('Enter new phrase, or type Exit to quit'))
 
 # Run
 if __name__ == "__main__":
     args = str(sys.argv)
-    print(pipeline("Pepper is a very ugly cat. Paul is a very cool dude"))
+    pipeline()
     
