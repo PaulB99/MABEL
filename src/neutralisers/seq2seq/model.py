74dc30bc14a1b785de1e5dbb4ecf19e4c419ce14
Paul <paulbyrne99@rocketmail.com>
2021-04-14 02:48:11 +0100

    seq2seq model added
    Former-commit-id: b2eb3c398bfc21353e11bfb606f2441da79d195c

--- src/neutralisers/seq2seq/model.py
+++ src/neutralisers/seq2seq/model.py
@@ -1,1 +1,59 @@
+import torch.nn as nn
+import torch
+import encoder
+import decoder
+import random
+
 # The seq2seq model
+
+# Start and end of sequence tokens
+SOS_token = 0
+EOS_token = 1
+
+class seq2seq():
+    
+    def __init__(self, encoder, decoder, device):
+        
+       super().__init__()
+      
+        # Initialise the encoder and decoder
+       self.encoder = encoder
+       self.decoder = decoder
+       self.device = device       
+      
+     # Forward function
+    def forward(self, text, target, teacher_forcing_ratio=0.5):
+
+        # Get input size
+        input_length = text.size(0)
+        batch_size = target.shape[1] 
+        target_length = target.shape[0]
+        vocab_size = self.decoder.output_dim
+          
+        # Initialise variable for predictions
+        outputs = torch.zeros(target_length, batch_size, vocab_size).to(self.device)
+    
+        # Encode each word
+        for i in range(input_length):
+            encoder_output, encoder_hidden = self.encoder(text[i])
+    
+        # Use the encoder’s hidden layer as the decoder's hidden
+        decoder_hidden = encoder_hidden.to(self.device)
+      
+        # Add start of sequence token
+        decoder_input = torch.tensor([SOS_token], device=self.device)
+    
+        # For each word, run decoder and make predictions
+        for t in range(target_length):   
+            
+            decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+            outputs[t] = decoder_output
+            
+            # If teacher forcing, next decoder input is the next word. If not, use the highest value from decoder
+            teacher_force = random.random() < teacher_forcing_ratio
+            topv, topi = decoder_output.topk(1)
+            input = (target[t] if teacher_force else topi)
+            if(teacher_force == False and input.item() == EOS_token):
+                break
+    
+        return outputs
