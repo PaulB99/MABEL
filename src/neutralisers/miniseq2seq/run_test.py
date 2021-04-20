9e5ac76b43f86c2137cb560753b11084b4d3af19
Paul <paulbyrne99@rocketmail.com>
2021-04-20 04:23:01 +0100

    seq2seq training change
    Former-commit-id: aeb1567993695c2846b26d7b3b8ef5b9d5c844ba

--- src/neutralisers/miniseq2seq/run_test.py
+++ src/neutralisers/miniseq2seq/run_test.py
@@ -1,9 +1,11 @@
 import unittest
 import sys
 sys.path.insert(0, '../')
-import model
+import model, train
 import torch
 from transformers import BertTokenizer
+import torch.nn as nn
+import torch.optim as optim
 
 # Test miniseq2seq functions
 class TestMiniSeq2Seq(unittest.TestCase):
@@ -23,6 +25,24 @@ class TestMiniSeq2Seq(unittest.TestCase):
         output_tensor = tok.decode(input_tensor)
         self.assertEqual(input_tensor, output_tensor)
 
+    def test_flow(self):
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        tok=BertTokenizer("bert-base-uncased-vocab-mini.txt")
+        mod = model.seq2seq(device, 7630)
+        output1 = mod.generate('Hello world', tok)
+        #self.assertEqual(output1, 'Hello world')
+        input_tensor = tok.encode('Hello world', return_tensors="pt")[0].to(device)
+        input_tensor = input_tensor.view(-1, 1)
+        target_tensor = tok.encode('Hello world', return_tensors="pt")[0].to(device)
+        target_tensor = input_tensor.view(-1, 1)
+        optimiser = optim.SGD(mod.parameters(), lr=0.01)
+        criterion = nn.NLLLoss()
+
+        for i in range(10):
+            loss = train.train_step(mod, input_tensor, target_tensor, optimiser, criterion)
+        output2 = mod.generate('Hello world', tok)
+        self.assertNotEqual(output1, output2)
+        
 
 if __name__ == '__main__':
     
