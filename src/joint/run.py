10829bf5897c368113165f113ffb4cb8808183ea
Paul <paulbyrne99@rocketmail.com>
2021-05-05 21:54:18 +0100

    silly mistake
    Former-commit-id: 32e585fbfc9060071e1b612a199cd8bc2760a7e8

--- src/joint/run.py
+++ src/joint/run.py
@@ -4,7 +4,7 @@ import time
 sys.path.insert(0, '../')
 from taggers.base_model import model as base_model
 from taggers.large_model import model as large_model
-from tagger.distilBERT import model as distil_model
+from taggers.distilBERT import model as distil_model
 from taggers.lexi import model as lexi_model
 from neutralisers.parrot import model as parrot_model
 from neutralisers.seq2seq import model as seq2seq_model
